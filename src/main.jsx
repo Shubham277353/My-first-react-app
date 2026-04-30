f0239c01465ecbea1a3a0719e360a39d015ef3bc
Shubham277353 <golubhandari405@gmail.com>
2026-04-30 22:26:13 +0530

    learn conditonal rendering like ternary , && etc.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Prop from './practice.jsx'
 import App from './App.jsx'
-import Rendering from './rendering.jsx'
+import PackingList from './rendering.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Rendering/>
+    <PackingList/>
   </StrictMode>
 )
--- src/rendering.jsx
+++ src/rendering.jsx
@@ -1,14 +1,37 @@
-export default function Rendering(){
-    const animals = ["Tiger", "Cow", "Monkey", "Gorilla", "Wolf"];
+function Item({ name, isPacked }) {
+  let itemContent = name ;
 
-    return (
-        <div>
-            <h1>Animals: </h1>
-            <ul>
-                {animals.map((animal)=>{
-                    return <li key={animal}>{animal}</li>;
-                })}
-            </ul>
-        </div>
+  if(isPacked){
+    itemContent = (
+        <del>
+            {name + " ✓"}
+        </del>
     );
+  }
+
+  return (
+    <li className = "item">{itemContent}</li>
+  )
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
+  );
 }
