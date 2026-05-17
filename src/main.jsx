48fda481bd87920939ad42eba2ad9fd53de994ea
Shubham277353 <golubhandari405@gmail.com>
2026-05-17 12:58:25 +0530

    learn react states in depth

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import Prop from './practice.jsx'
 import App from './App.jsx'
 import RecipeList from './rendering.jsx'
 import Counter from './useState.jsx'
-import ReactWebpage from './reactApp.jsx'
+import TrafficLight from './trafficLightSim.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ReactWebpage title={"My first react App"} phoneNo={4239874194} />
+    <TrafficLight/>
   </StrictMode>
 )
--- src/reactApp.jsx
+++ src/reactApp.jsx
@@ -1,48 +1,19 @@
 import { useState } from "react";
 
-export default function ReactWebpage({title,phoneNo}) {
-    const [counter,setCounter] = useState(0);
-    const [colour , setColour] = useState("black");
+export default function Person() {
+  const [person, setPerson] = useState({ name: "John", age: 100 });
 
-    
-    const addition = () => {
-        setCounter(counter + 1);
-        changeColour();
-    }
-    
-    const subtract = () =>{
-        setCounter(counter -1);
-        changeColour();
-    }
+  const handleIncreaseAge = () => {
+    setPerson((prev)=>({ ...prev, age: prev.age + 1 }));
+    setPerson((prev)=>({ ...prev, age: prev.age + 1 }));
+  };
 
-    const changeColour = () => {
-        if(counter % 2 == 0){
-            setColour("red");
-        }else{
-            setColour("blue");
-        }
-    }
 
   return (
-    <div style={{backgroundColor: colour}}>
-      <nav>
-        <h1>{title}</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <main>
-        <h2>Welcome to my website</h2>
-        <p>This is my first react webpage</p>
-        <button onClick={subtract}>Subtract</button>
-        <h2>{counter}</h2>
-        <button onClick={addition}>Add</button>
-      </main>
-      <footer>
-        contact me : {phoneNo}
-      </footer>
-    </div>
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </>
   );
 }
